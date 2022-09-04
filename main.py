5f56d2dd49f8fd8acbb08b9a40521ceee9901a5a
Heylo234 <111825278+Heylo234@users.noreply.github.com>
2022-09-04 17:50:30 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+sign = input("What calculation should I do?(e.g subtraction, addition)")
+first = input("First Number>")
+second = input("Second Number>")
+if sign == "addition":
+    print("sum:")
+    print(int(first) + int(second))
+else:
+    if sign == "subtraction":
+        print("difference:")
+        print(int(first) - int(second))
+    else:
+        if sign == "multiplication":
+            print("product:")
+            print(int(first) * int(second))
+        else:
+            if sign == "division":
+                print("quotient:")
+                print(int(first) / int(second))
+            else:
+                print("Something when wrong! Try again.")
+print("Decimals coming soon!")
